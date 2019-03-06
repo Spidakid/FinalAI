f2ee1eb468c0c33853a5faa00e56464baaf8b331
Lavance Strickland <lavance_strickland@yahoo.com>
2019-03-05 23:50:11 -0600

    Initial Commit

---
+++ Assets/Scripts/SortedList.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortedList
+{
+    private ArrayList list = new ArrayList();
+    public int Length
+    {
+        get { return list.Count; }
+    }
+    public void Push(Node _node)
+    {
+        list.Add(_node);
+        list.Sort();
+    }
+    public void Remove(Node _node)
+    {
+        list.Remove(_node);
+        list.Sort();
+    }
+    public bool Contains(Node _node)
+    {
+        return list.Contains(_node);
+    }
+    public Node GetNode(int index)
+    {
+        return (Node)list[index];
+    }
+}
