9a65ab8479717462f2e97380d0eebf4ede5f2d24
Lavance Strickland <lavance_strickland@yahoo.com>
2019-03-17 23:07:31 -0500

    Added path folowing and steering to the A* Navigation script; added Gizmos to visually represent nodes along the grid;

--- Assets/Scripts/Pathfinding/EmptyObj/AStarNavigation.cs
+++ Assets/Scripts/Pathfinding/EmptyObj/AStarNavigation.cs
@@ -12,7 +12,9 @@ public class AStarNavigation : MonoBehaviour
     private float currentTime = 0.0f;
     public float intervalTime = 1.0f; //Interval time between path finding
     public float Speed = 1.0f;
-    public Color pathColor = Color.yellow;
+    public Color lineColor = Color.yellow;
+    public float stopRadius = 0.5f;
+    public Color sphereColor = Color.magenta;
     private int curPathindex;
     // Use this for initialization
     void Start()
@@ -26,17 +28,23 @@ public class AStarNavigation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Timer
-        currentTime += Time.deltaTime;
-        //Finds a path every interval
-        if (currentTime >= intervalTime)
+        //Check if current index is within the bounds of the pathArray
+        if (curPathindex < pathArray.Count)
         {
-            currentTime = 0.0f;
-            FindPath();
+            //Timer
+            currentTime += Time.deltaTime;
+            //Finds a path every interval
+            if (currentTime >= intervalTime)
+            {
+                Debug.Log("RESET!");
+                ResetPathfinding();
+            }
+            FollowPath();
         }
-        
     }
-
+    /// <summary>
+    /// Finds a new path
+    /// </summary>
     void FindPath()
     {
         startNode = GetNodeForGrid(startPos.position);
@@ -48,6 +56,47 @@ public class AStarNavigation : MonoBehaviour
     {
         return new Node(GridManager.Instance.GetGridTileCenter(GridManager.Instance.GetGridIndex(_pos)));
     }
+    /// <summary>
+    /// Follow and Steer towards the current generated path
+    /// </summary>
+    void FollowPath()
+    {
+        //ternary operator assignment
+        //  if curPathIndex < pathArray.Count then curNode = (Node)pathArray[curPathindex];
+        //  else curNode = null;
+        Node curNode = (curPathindex < pathArray.Count) ? (Node)pathArray[curPathindex] : null;
+        //Check if array or current node is null
+        if (pathArray == null ||curNode == null)
+        {
+            return;
+        }
+        else if (Vector3.Distance(this.transform.position, curNode.position) < this.stopRadius)
+        {
+            curPathindex++;
+            //Reset index to 0 if out of bounds
+            if (curPathindex >= pathArray.Count)
+            {
+                curPathindex = 0;
+            }
+        }
+        Vector3 objToCurNode = curNode.position - this.transform.position;
+        this.transform.rotation = Quaternion.LookRotation(objToCurNode);//Rotate Agent to face node
+        this.transform.Translate(Vector3.forward * Speed * Time.deltaTime);//Move Agent
+        
+
+    }
+    /// <summary>
+    /// Restart the Pathfinding process
+    /// </summary>
+    void ResetPathfinding()
+    {
+        //reset refresh timer
+        currentTime = 0.0f;
+        
+        //Generate new A* path
+        FindPath();
+    }
+    #region Debug
     void OnDrawGizmos()
     {
         Debug.DrawLine(this.transform.position, this.transform.position + this.transform.forward * 3, Color.red);
@@ -62,11 +111,14 @@ public class AStarNavigation : MonoBehaviour
                 if (index < pathArray.Count)
                 {
                     Node nextNode = (Node)pathArray[index];
-                    Debug.DrawLine(node.position, nextNode.position, pathColor);
+                    Debug.DrawLine(node.position, nextNode.position, lineColor);
+                    //Sphere Debug Drawing
+                    Gizmos.color = sphereColor;
+                    Gizmos.DrawSphere(node.position,stopRadius);
                     index++;
                 }
             };
         }
-        
     }
+    #endregion
 }
